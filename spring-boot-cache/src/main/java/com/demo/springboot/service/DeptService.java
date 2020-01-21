2b7aa24dd10f29430cf7aafb65824f500a957a1c
Jay-Jiang <1540491116@qq.com>
2020-01-21 09:03:08 +0800

    [修改代码] 修改部分缓存注解表达式，添加注释信息

--- spring-boot-cache/src/main/java/com/demo/springboot/service/DeptService.java
+++ spring-boot-cache/src/main/java/com/demo/springboot/service/DeptService.java
@@ -38,7 +38,7 @@ public class DeptService {
      *
      * @param id
      */
-    @CacheEvict(cacheNames = "{deptCache}", key = "\"dept_\"+#id", beforeInvocation = true)
+    @CacheEvict(cacheNames = "{deptCache}", key = "'dept_'+#id", beforeInvocation = true)
     public int removeDept(Long id) {
         //默认情况下，即 beforeInvocation=false，如果方法发生异常中断，则不会执行缓存清除
         try {
@@ -55,12 +55,12 @@ public class DeptService {
     /**
      * 更新缓存
      * 注意：此方法的返回值，应该与缓存中的结果类型保持一致
-     * 不能一个是 int，一个是 Department 对象
-     * 通过 condition 将更新与否和返回值绑定
+     * 不能一个是 int，一个是 Department 对象，否则，在重新获取缓存结果时，可能导致类型转换异常
+     * 通过 condition 将更新与否和返回值绑定，当返回值有效时，才会更新缓存
      *
      * @param dept
      */
-    @CachePut(cacheNames = "{deptCache}", key = "\"dept_\"+#dept.getId()"/*, unless = "#result == null"*/)
+    @CachePut(cacheNames = "{deptCache}", key = "'dept_'+#dept.getId()", unless = "#result == null")
     public Department updateDept(Department dept) {
         int result = deptMapper.updateDeptById(dept);
         if (result > 0) {
@@ -77,12 +77,22 @@ public class DeptService {
     /**
      * 缓存中没有，执行方法，将返回值添加到缓存中
      * 缓存中有，不再执行方法，直接返回缓存中的值
-     *
-     * @param id
+     * 底层实现步骤：
+     *   1. 在调用目标方法之前，CacheManager 按照 cacheNames 查找对应的 Cache 实例
+     *     - 如果没有查找到，则按照给定的 cacheName 创建一个 Cache 实例，并返回它
+     *   2. 从获取到的 Cache 实例中，按照给定的 Key 查找对应的缓存数据
+     *      - 如果找到了，直接返回该缓存数据
+     *      - 如果没找到，则调用目标方法，并将方法的返回值，按照给定的 key 保存到指定 Cache 实例中
+     * 核心：
+     *   1. 没有配置的情况下，默认使用 ConcurrentMapCacheManager 和 ConcurrentMapCache
+     *   2. 缓存数据 key ，没有指定时，默认使用 SimpleKeyGenerator 来生成
+     *      - 如下，使用 SpEL 表达式，来自定义 key
+     *      - 拼接字符串时，可以使用 \" 或 ' 两种形式，
+     *        - 即 key = "\"dept_\"+#id" 或者 key = "'dept_'+#id" ===> dept_2
      */
-    @Cacheable(cacheNames = "{deptCache}", key = "\"dept_\"+#id", condition = "#id % 2 == 0")
+    @Cacheable(cacheNames = "{deptCache}", key = "'dept_'+#id", condition = "#id % 2 == 0")
     public Department getDept(Long id) {
-        log.info("本次通过数据库来获取 dept 信息，id=[{}]", id);
+        log.info("本次通过数据库来获取 dept[id={}] 信息。", id);
         return deptMapper.selectDeptById(id);
     }
 
